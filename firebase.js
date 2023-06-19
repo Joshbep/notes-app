664690a902317a9a4b5eec081f0cc4416333b451
jbep <joshuabemartin@gmail.com>
2023-06-19 18:35:41 -0400

    fix: ixed authentication in firestorE

--- firebase.js
+++ firebase.js
@@ -14,3 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const notesCollection = collection(db, "notes")
+
+//edited authentication in firestore
